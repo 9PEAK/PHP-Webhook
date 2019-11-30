c44601ea77690a11de78a649d6b39c02c9b125bf
Kane <52653784@qq.com>
2019-11-30 22:46:38 +0800

    #

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include 'lib.inc.php';
 $shell = new Core($_GET['id']);
 
 if ($shell->test()) {
+    echo $shell->error();
     print_r($shell->exec('string'));
 } else {
     echo '[ERROR] '.$shell->error();
