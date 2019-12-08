f437a2635342b5b7ca8fe8989ec66599a9804767
Kane <52653784@qq.com>
2019-12-08 12:59:59 +0800

    #

--- test.php
+++ test.php
@@ -3,8 +3,10 @@
 //echo 'Composer!!!';
 //exit;
 
-$res = null;
-passthru('/data/wwwroot/huba.yun.9peak.net/test.sh', $res);
+$cmd = '/data/wwwroot/huba.yun.9peak.net/test.sh';
+echo exec($cmd);
+//$res = null;
+//passthru($cmd, $res);
+//echo $res;
 
-echo $res;
 
