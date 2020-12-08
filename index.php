1e936444241e117ef19edadf916031f9d4156cad
Kane <52653784@qq.com>
2020-12-08 15:58:44 +0800

    #

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-include 'common/config.php';
 include 'common/req.php';
 include 'git/core.php';
 
@@ -11,25 +10,21 @@ try {
     if (!$config = @$config[$_GET['id']]) {
         throw new Exception('Webhook未配置。');
     }
-    $config = Config::set($config);
-
-    print_r($_SERVER);
 
     # 创建处理器
-//    if (!$git = Core::factory($config->typ)) {
-//        throw new Exception('暂不支持该GIT仓库。');
-//    }
+    if (!$git = Core::factory($config['typ'])) {
+        throw new Exception('暂不支持该GIT仓库。');
+    }
 
-    # 创建处理器、校验KEY、执行脚本
-    $git = Core::factory($config->typ);
-    if (!$git->auth($config->key, req_header(), (array)req_body())) {
+    # 校验KEY
+    if (!$git->auth($config['key'], req_header(), (array)req_body())) {
         throw new Exception('KEY校验失败。');
     }
 
-    $res = $git->exec($config->cmd);
+    # 执行脚本
+    $res = $git->exec($config['cmd']);
 
     echo "[SUCCESS] 执行完毕：\n";
-//    print_r($res);
 
 } catch (Exception $e) {
 
