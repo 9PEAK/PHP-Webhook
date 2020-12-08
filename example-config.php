64db02948104f83acc5bb6ef566806cd0da5cc86
Kane <52653784@qq.com>
2020-12-08 18:19:30 +0800

    #

--- example-config.php
+++ example-config.php
@@ -4,8 +4,6 @@ return [
     'webhook' => [
         'key'=> 'as-sk-token-sign',
         'typ'=> 'github',
-        'msg' => '# GIT DEPLOY',
-        'dir'=> '/linux/www/yun.abc.net/',
         'cmd'=> [
             'git pull'
         ]
