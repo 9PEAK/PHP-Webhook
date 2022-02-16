46b00682dae115a93deae67bb834958936a54775
Kane <52653784>
2022-02-16 15:23:04 +0800

    #

--- index.php
+++ index.php
@@ -28,8 +28,8 @@ try {
     }
     $log->info('body');
     $log->info(json_encode($body) );
-//    $sign = 'sha1='.hash_hmac('sha1', json_encode($body), '702F05186DC10B740604D923E5BAE669');
-    $sign = 'sha1='.sha1('702F05186DC10B740604D923E5BAE669'. json_encode($body));
+    $sign = 'sha1='.hash_hmac('sha1', is_string($body) ? $body : json_encode($body), '702F05186DC10B740604D923E5BAE669');
+//    $sign = 'sha1='.sha1('702F05186DC10B740604D923E5BAE669'. json_encode($body));
     $log->info('签名： '. $sign);
 
     # 校验KEY
